853b3ce5472ff512eb49eeb04ca3312dca80a475
Carter Hemington <carter.hemington@gmail.com>
2022-02-14 15:56:38 +0000

    add favouritesPage

---
+++ routes/favourites.js
@@ -0,0 +1,25 @@
+/*
+ * All routes for favourites are defined here
+ * Since this file is loaded in server.js into api/favourites,
+ *   these routes are mounted onto /favourites
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM favourites;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
