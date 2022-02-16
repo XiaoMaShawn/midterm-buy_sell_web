3e458be3dc296f598c23e5436c0c2ebeef140d04
Carter Hemington <carter.hemington@gmail.com>
2022-02-16 16:32:29 +0000

    chat css change

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,46 @@
-// DO WE HAVE to require jquery
+// <!-- link JS file and jQuery -->
+//client.js and Chat.ejs are linked
+
+//do i need to link to CHAT.ejs
+//do i need to require jquery
+
+<script type="text/javascript" src="/vendor/jquery-3.0.0.js"></script>
 
 $(document).ready(function() {
-$().on("")
+  //form submission
+  //SYNTAX REMINDER # is id, . is class
+$("#submit-form").submit(function(event) {
+  //avoid page reload
+  event.preventDefault()
+
+  //get the message
+  let getMessage = $('input').val()
+
+  //if the message IS NOT empty
+  if(getMessage !== '') {
+    //prep
+    //area to append is CLASS MESSAGE
+    let newMessage = "<p class ='message' >" + getMessage + "</p> ";
+
+    //add/append the message to the box
+    $('.PLACETOPUT').append(newMessage);
+
+  //clearing the form
+    $('input').val("")
+  }
+  db.query(
+    //this needs to be an insert query are they compatible
+    `QUERY`
+  )
+    .then((data) => {
+      const favourites = data.rows;
+      const templateVars = {};
+      templateVars.favourites = favourites;
+      res.render("favourites", templateVars);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+})
 
 });
