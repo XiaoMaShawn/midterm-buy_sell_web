9a78fa4327fcdf6e7f39d79ef2721fe10c848b08
Carter Hemington <carter.hemington@gmail.com>
2022-02-17 21:12:00 +0000

    chat now appends to page

--- db/seeds/02_messages.sql
+++ db/seeds/02_messages.sql
@@ -1,3 +1,10 @@
 INSERT INTO messages (content, title, from_id, item_id) VALUES ('Hi, wondering if this is still available for purchase?','Is this available',  1, 2);
 INSERT INTO messages (content, title, from_id, item_id) VALUES ('We were supposed to meet but you never showed','HELLO', 3, 1);
-INSERT INTO messages (content, title, from_id, item_id) VALUES ('That price looks a little steep - must be in great condition. Can you send me more photos please','Price',  4, 3);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('That price looks a little steep - must be in great condition. Can you send me more photos please','Price',  1, 4);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('Would you take a lower offer?','Open to negotiation?',  2, 1);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('You have not responded to me yet - why?','PLZ respond',  4, 1);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('This looks way to valuable to be listed for this price','Did you mean to list this?',  4, 2);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('This looks too good to be true - is it?','Too good',  3, 1);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('Why did you want to meet there','Location',  2, 3);
+INSERT INTO messages (content, title, from_id, item_id) VALUES ('That price looks a little steep - must be in great condition. Can you send me more photos please','Price',  2, 1);
+
--- routes/messages.js
+++ routes/messages.js
@@ -10,11 +10,14 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+
     db.query(
       `
     SELECT *
     FROM messages
-    JOIN items ON items.id = messages.item_id;
+    JOIN users ON users.id = messages.from_id
+    JOIN items ON items.id = messages.item_id
+
     `
     )
       .then((data) => {
@@ -29,59 +32,83 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+//CHANGE TO BE MADE: need to filter to items that tHIS PERSON owns
+
 
   router.get("/:id", (req, res) => {
     db.query(
       `
-    SELECT messages.id AS message_id, messages.title AS title, messages.content AS content, messages.sent_at AS sent_at, items.name AS item_name, users.name AS owner
+    SELECT
+    messages.id AS message_id,
+    messages.title AS title,
+    messages.from_id AS from_id,
+    messages.content AS content,
+    messages.sent_at AS sent_at,
+    items.name AS item_name,
+    users.name AS owner,
+    messages.item_id as item
     FROM messages
     JOIN items ON items.id = messages.item_id
     JOIN users ON owner_id = users.id
-    WHERE messages.id = ${req.params.id}
+    WHERE items.id = ${req.params.id}
     `
     ).then((data) => {
       const templateVars = {};
       templateVars.messages = data.rows;
+
       templateVars.username = req.session.username;
       templateVars.id = req.session.id;
 
-      // console.log(templateVars);
+
       res.render("messagesID", templateVars);
     });
   });
 
   router.get("/:id/chat", (req, res) => {
-    db.query(
+  console.log("req.params.id", req.param.id)
+  db.query(
+
       `
-    SELECT messages.id AS message_id, messages.title AS title, messages.content AS content, messages.sent_at AS sent_at, items.name AS item_name, users.name AS owner
+    SELECT
+    messages.id AS message_id,
+    messages.title AS title,
+    messages.from_id AS from_id,
+    messages.content AS content,
+    messages.sent_at AS sent_at,
+    items.name AS item_name,
+    users.name AS owner,
+    messages.item_id as item
     FROM messages
     JOIN items ON items.id = messages.item_id
     JOIN users ON owner_id = users.id
-    WHERE messages.id = ${req.params.id}
+    WHERE messages.item_id = ${req.params.id}
     `
     ).then((data) => {
-      const templateVars = {
-        messages: data.rows,
-      };
-      // data.rows;
-      // console.log(templateVars);
+      const templateVars = {};
+      templateVars.messages = data.rows,
+        templateVars.username = req.session.username;
+      templateVars.id = req.session.id;
+
       res.render("chat", templateVars);
       // res.json({messages:templateVar.messages})
     });
   });
 
   router.post("/:id/chat", (req, res) => {
-    console.log(req.body);
-    db.query(
+console.log("req.body", req.body)
+// console.log("req.body.from_id", req.body.from_id)
+// console.log("req.body.item_id", req.body.item_id)
+
+      db.query(
       `INSERT INTO messages (content, title, from_id,  item_id) VALUES ($1, $2, $3, $4) RETURNING *
     `,
-      [req.body.content, "title1", 3, 1]
+      [req.body.content, req.body.title, req.body.from, req.body.item_id]
     ).then((data) => {
       const templateVars = {};
       templateVars.messages = data.rows;
       // console.log(templateVars);
       res.redirect("/messages");
-      // res.render('chat', templateVars)
+
     });
   });
 
